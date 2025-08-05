306ff801fed94e5ac3e2712692a11cc624b4fba1
Soham S Gumaste <sgumas2@uic.edu>
2025-08-05 18:57:52 -0500

    Init

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Shell script for uploading Cflare Pages/Static Assets";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                shellcheck
+		            dash
+                jq
+                zola
+              ];
+
+            };
+        };
+      flake = {
+      };
+    };
+}
